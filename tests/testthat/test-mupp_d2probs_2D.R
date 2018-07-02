8ae62c8b2af06a28eae06d518ec84139cc08d863
Steven Nydick <steven.nydick@kornferry.com>
2018-07-02 16:02:33 -0500

    Add unit tests
    Unit tests for second derivative of probability functions, derivative of likelihood, and second derivative of likelihood.

---
+++ tests/testthat/test-mupp_d2probs_2D.R
@@ -0,0 +1,256 @@
+context("test-mupp_d2probs_2D.R")
+
+test_that("mupp probability second derivatives work: two dimensions", {
+
+  # 0 # Misc Objects
+  dims   <- 1:2
+  n_dims <- length(dims)
+
+  # 0 # R functions to calculate probs
+
+  # - GGUM
+  p_ggum <- function(theta, alpha, delta, tau){
+    e1 <- exp(alpha * ((theta - delta) - tau))
+    e2 <- exp(alpha * (2 * (theta - delta) - tau))
+    e3 <- exp(alpha * (3 * (theta - delta)))
+    (e1 + e2) / (1 + e1 + e2 + e3)
+  } # END p_ggum FUNCTION
+
+  # - MUPP (for 2D only) (intentionally manual and hard coded)
+  p_mupp <- function(thetas, params){
+    p_s <- p_ggum(thetas[ , 1], params[1, 1], params[1, 2], params[1, 3])
+    p_t <- p_ggum(thetas[ , 2], params[2, 1], params[2, 2], params[2, 3])
+    p   <- p_s * (1 - p_t) / (p_s * (1 - p_t) + p_t * (1 - p_s))
+    P   <- "dimnames<-"(cbind(p, 1 - p), NULL)
+    P
+  } # END p_mupp (2D) FUNCTION
+
+  # 0 # R functions to calculate derivatives
+  pder1_ggum <- function(theta, alpha, delta, tau){
+    e1 <- exp(alpha * ((theta - delta) - tau))
+    e2 <- exp(alpha * (2 * (theta - delta) - tau))
+    e3 <- exp(alpha * (3 * (theta - delta)))
+    alpha * (e1 * (1 - 2 * e3) + e2 * (2 - e3)) / (1 + e1 + e2 + e3)^2
+  }
+
+  # 0 # R functions to calculate second derivatives
+  pder2_ggum <- function(theta, alpha, delta, tau){
+    e1 <- exp(alpha * ((theta - delta) - tau))
+    e2 <- exp(alpha * (2 * (theta - delta) - tau))
+    e3 <- exp(alpha * (3 * (theta - delta)))
+
+    A  <- alpha   * (e1 * (1 - 2 * e3) + e2 * (2 - e3))
+    dA <- alpha^2 * (e1 * (1 - 8 * e3) + e2 * (4 - 5 * e3))
+    B  <- (1 + e1 + e2 + e3) ^ 2
+    dB <- 2 * alpha * (1 + e1 + e2 + e3) * (e1 + 2 * e2 + 3 * e3)
+    (B * dA - A * dB) / (B ^ 2)
+  } # END pder2_ggum FUNCTION
+
+  # - MUPP (for 2D only) (intentionally manual and hard coded and NOT simplified)
+  #                      (same as in paper, even though paper could be simplified more)
+  pder2_mupp <- function(thetas, params){
+    A <- p_ggum(thetas[ , 1], params[1, 1], params[1, 2], params[1, 3])
+    B <- 1 - p_ggum(thetas[ , 2], params[2, 1], params[2, 2], params[2, 3])
+    C <- 1 - A
+    D <- 1 - B
+
+    Ap <-  pder1_ggum(thetas[ , 1], params[1, 1], params[1, 2], params[1, 3])
+    Bp <- -pder1_ggum(thetas[ , 2], params[2, 1], params[2, 2], params[2, 3])
+
+    A2p <-  pder2_ggum(thetas[ , 1], params[1, 1], params[1, 2], params[1, 3])
+    B2p <- -pder2_ggum(thetas[ , 2], params[2, 1], params[2, 2], params[2, 3])
+
+    d1  <- B * (1 - B) * (A2p *  (A * B + C * D) - 2 * Ap^2 * (2 * B - 1)) / (A * B + C * D)^3
+    d2  <- A * (1 - A) * (B2p *  (A * B + C * D) - 2 * Bp^2 * (2 * A - 1)) / (A * B + C * D)^3
+    d12 <- Ap * Bp * (1 - A - B) / (A * B + C * D)^3
+
+    dP <- "dimnames<-"(cbind(d1, d2, d12), NULL)
+    dP
+  } # END pder2_mupp FUNCTION
+
+  # 0 # R function to put second derivatives in correct order
+  pder2_mupp_all <- function(thetas, params){
+    d1 <- pder2_mupp(thetas, params)
+    d2 <- pder2_mupp(thetas[ , 2:1, drop = FALSE],
+                     params[2:1, , drop = FALSE])[ , c(2:1, 3), drop = FALSE]
+    dP <- "dimnames<-"(list(d1, d2), NULL)
+    dP
+  }
+
+  # 0 # Misc Functions
+  convert_hessian <- function(mat){
+    rbind(c(diag(mat), mat[lower.tri(mat, diag = FALSE)]))
+  }
+
+  # b # change theta but keep items the same
+  thetas <- rbind(c(0, -1))
+  alphas <- c(1, 1)
+  deltas <- c(0, 0)
+  taus   <- c(0, 0)
+  params <- cbind(alphas, deltas, taus)
+
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims),
+               pder2_mupp_all(thetas, params))
+
+  # c # change alpha but keep items the same
+  thetas <- rbind(c(0, 0))
+  alphas <- c(1, 2)
+  params <- cbind(alphas, deltas, taus)
+
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims),
+               pder2_mupp_all(thetas, params))
+
+  # d # change delta but keep items the same
+  alphas <- c(1, 1)
+  deltas <- c(0, -1)
+  params <- cbind(alphas, deltas, taus)
+
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims),
+               pder2_mupp_all(thetas, params))
+
+  # d # change taus but keep items the same
+  deltas <- c(0, 0)
+  taus   <- c(0, -1)
+  params <- cbind(alphas, deltas, taus)
+
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims),
+               pder2_mupp_all(thetas, params))
+
+  # d # change everything
+  thetas <- rbind(c(0, 1))
+  alphas <- c(1, 2)
+  deltas <- c(0, -1)
+  taus   <- c(0, -1)
+  params <- cbind(alphas, deltas, taus)
+
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims),
+               pder2_mupp_all(thetas, params))
+
+  # e # multiple people RANDOM, so different each time :)
+  n_thetas <- 5
+  thetas   <- matrix(r_thetas_prior(n_thetas * n_dims),
+                     nrow = n_thetas,
+                     ncol = n_dims)
+  alphas   <- r_alpha_prior(n_dims)
+  deltas   <- r_delta_prior(n_dims)
+  taus     <- r_tau_prior(n_dims)
+
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims),
+               pder2_mupp_all(thetas, params))
+
+  # f # selecting one dimension
+
+  # - first dimension - same for everyone
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims,
+                                    picked_order_id = 1),
+               pder2_mupp_all(thetas, params)[1])
+
+  # - second dimension - same for everyone
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims,
+                                    picked_order_id = 2),
+               pder2_mupp_all(thetas, params)[2])
+
+  # - first/second dimension altering
+  ids    <- rep(dims, length.out = n_thetas)
+  pder2  <- pder2_mupp_all(thetas, params)
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims,
+                                    picked_order_id = ids),
+               list(do.call(what = rbind,
+                            args = lapply(seq_along(ids),
+                                          FUN = function(i)
+                                            pder2[[ids[i]]][i, , drop = FALSE]))))
+
+  # - first/second dimension random order
+  ids    <- sample(dims, size = n_thetas, replace = TRUE)
+  pder2  <- pder2_mupp_all(thetas, params)
+  expect_equal(pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = dims,
+                                    picked_order_id = ids),
+               list(do.call(what = rbind,
+                            args = lapply(seq_along(ids),
+                                          FUN = function(i)
+                                            pder2[[ids[i]]][i, , drop = FALSE]))))
+
+  # g # using grad from numDeriv
+
+  # - first dimension, first item picked
+  thetas    <- thetas[1, , drop = FALSE]
+  pder2     <- numDeriv::hessian(func = function(x){
+                                   p_mupp_rank_impl(thetas = x,
+                                                    params = params,
+                                                    dims   = c(1, 1),
+                                                    picked_order_id = 1)
+                                 },
+                                 x = thetas)
+  pder2_a   <- pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = c(1, 1),
+                                    picked_order_id = 1)[[1]]
+  expect_equivalent(all(abs(convert_hessian(pder2) - pder2_a) < 1e-09),
+                    TRUE)
+
+  # - first dimension, second item picked
+  pder2     <- numDeriv::hessian(func = function(x){
+                                   p_mupp_rank_impl(thetas = x,
+                                                    params = params,
+                                                    dims   = c(1, 1),
+                                                    picked_order_id = 2)
+                                 },
+                                 x = thetas)
+  pder2_a   <- pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = c(1, 1),
+                                    picked_order_id = 2)[[1]]
+  expect_equivalent(all(abs(convert_hessian(pder2) - pder2_a) < 1e-09),
+                    TRUE)
+
+  # - second dimension, first item picked
+  pder2     <- numDeriv::hessian(func = function(x){
+                                   p_mupp_rank_impl(thetas = x,
+                                                    params = params,
+                                                    dims   = c(2, 2),
+                                                    picked_order_id = 1)
+                                 },
+                                 x = thetas)
+  pder2_a   <- pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = c(2, 2),
+                                    picked_order_id = 1)[[1]]
+  expect_equivalent(all(abs(convert_hessian(pder2) - pder2_a) < 1e-09),
+                    TRUE)
+
+  # - second dimension, second item picked
+  pder2     <- numDeriv::hessian(func = function(x){
+                                   p_mupp_rank_impl(thetas = x,
+                                                    params = params,
+                                                    dims   = c(2, 2),
+                                                    picked_order_id = 2)
+                                 },
+                                 x = thetas)
+  pder2_a   <- pder2_mupp_rank_impl(thetas = thetas,
+                                    params = params,
+                                    dims   = c(2, 2),
+                                    picked_order_id = 2)[[1]]
+  expect_equivalent(all(abs(convert_hessian(pder2) - pder2_a) < 1e-09),
+                    TRUE)
+
+})
---
+++ tests/testthat/test-mupp_dlikelihood.R
@@ -0,0 +1,308 @@
+context("test-mupp_dlikelihood.R")
+
+test_that("mupp likelihood derivatives work", {
+
+  # 0 # R functions to calculate likelihoods
+
+  # turn parameters into useful objects for MUPP
+  transform_params_to_mupp <- function(params, resp){
+
+    # pulling out objects (thetas, items, params)
+    thetas <- as.matrix(tidyr::spread(params$persons,
+                                      key   = "dim",
+                                      value = "theta")[-1])
+    items  <- as.matrix(params$items[1:3])
+    params <- as.matrix(params$items[4:6])
+
+    # pulling out objects (resp)
+    resp   <- as.matrix(tidyr::spread(resp$resp[c("person", "item", "resp")],
+                                      key   = "item",
+                                      value = "resp")[-1])
+
+    return(list(thetas = thetas, params = params, items = items, resp = resp))
+  } # END transform_params_to_mupp FUNCTION
+
+  # calculating MUPP likelihood (NOT using C++ function)
+  estimate_mupp_dlikelihood <- function(thetas, params, items, resp){
+
+    g <- numDeriv::grad(func = function(x){
+                          sum(loglik_mupp_rank_impl(rbind(x),
+                                                    params,
+                                                    items,
+                                                    resp))
+                        }, x = thetas)
+    h <- numDeriv::hessian(func = function(x){
+                             sum(loglik_mupp_rank_impl(rbind(x),
+                                                       params,
+                                                       items,
+                                                       resp))
+                           }, x = thetas)
+
+    list(gradient = g,
+         hessian  = rbind(c(diag(h), h[lower.tri(h, diag = FALSE)])))
+  } # END calculate_mupp_dlikelihood FUNCTION
+
+  # creating list of possible responses (nPk permutations)
+  create_resp_list <- function(n_dims,
+                               n_comb){
+
+    # possible selections for choose matrix (allows permutations!)
+    select_opts <- rep(x     = seq_len(n_dims),
+                       times = n_comb)
+
+    # determine possible nPk permutations
+    mat <- t(combn(x = select_opts,
+                   m = n_comb,
+                   simplify = TRUE))
+    df  <- unique(as.data.frame(mat))
+
+    # arrange in useful order
+    df  <- df[do.call(base::order, args = df), ]
+
+    # turn into list and return
+    as.list(as.data.frame(t(df)))
+
+  } # END create_resp_list FUNCTION
+
+  # b # one item (one person, two dimensions)
+
+  n_items      <- 1
+  n_dims       <- 2
+
+  # calculating dims and stuff
+  dims         <- seq_len(n_dims)
+  n_statements <- n_items * n_dims
+  thetas       <- seq(from = 3, to = -3,
+                      length.out = n_dims)
+  alphas       <- rep(x = 1:2,
+                      length.out = n_statements)
+  deltas       <- seq(from = -2, to = 2,
+                      length.out = n_statements)
+  taus         <- rep(x = c(-1, 0, 1),
+                      length.out = n_statements)
+
+  # binding everything together
+  thetas <- rbind(thetas)
+  params <- cbind(alphas, deltas, taus)
+  items  <- cbind(item      = rep(x    = seq_len(n_statements / n_dims),
+                                  each = n_dims),
+                  statement = seq_len(n_statements),
+                  dim       = rep(dims,
+                                  times = n_items))
+  # creating response options
+  resp   <- create_resp_list(n_dims = n_dims,
+                             n_comb = n_items)
+
+  for(r in resp){
+
+    # estimating likelihood
+    lders   <- estimate_mupp_dlikelihood(thetas = thetas,
+                                         params = params,
+                                         items  = items,
+                                         resp   = rbind(r))
+    lder1   <- lders$gradient
+    lder2   <- lders$hessian
+
+    # actual likelihood
+    lder1_a <- lder1_mupp_rank_impl(thetas, params, items, rbind(r))
+    lder2_a <- lder2_mupp_rank_impl(thetas, params, items, rbind(r))
+
+    # comparing
+    expect_equivalent(all(abs(lder1 - lder1_a) < 1e-07),
+                      TRUE)
+    expect_equivalent(all(abs(lder2 - lder2_a) < 1e-07),
+                      TRUE)
+  } # END for r LOOP
+
+  # c # one item (one person, one dimension)
+  for(dim in 1:2){
+
+    n_items      <- 1
+    n_dims       <- 2
+
+    # calculating dims and stuff
+    dims         <- rep(dim, n_dims)
+    n_statements <- n_items * n_dims
+    thetas       <- seq(from = 3, to = -3,
+                        length.out = n_dims)
+    alphas       <- rep(x = 1:2,
+                        length.out = n_statements)
+    deltas       <- seq(from = -2, to = 2,
+                        length.out = n_statements)
+    taus         <- rep(x = c(-1, 0, 1),
+                        length.out = n_statements)
+
+    # binding everything together
+    thetas <- rbind(thetas)
+    params <- cbind(alphas, deltas, taus)
+    items  <- cbind(item      = rep(x    = seq_len(n_statements / n_dims),
+                                    each = n_dims),
+                    statement = seq_len(n_statements),
+                    dim       = rep(dims,
+                                    times = n_items))
+    # creating response options
+    resp   <- create_resp_list(n_dims = n_dims,
+                               n_comb = n_items)
+
+    for(r in resp){
+
+      # estimating likelihood
+      lders   <- estimate_mupp_dlikelihood(thetas = thetas,
+                                           params = params,
+                                           items  = items,
+                                           resp   = rbind(r))
+      lder1   <- lders$gradient
+      lder2   <- lders$hessian
+
+      # actual likelihood
+      lder1_a <- lder1_mupp_rank_impl(thetas, params, items, rbind(r))
+      lder2_a <- lder2_mupp_rank_impl(thetas, params, items, rbind(r))
+
+      # comparing
+      expect_equivalent(all(abs(lder1 - lder1_a) < 1e-07),
+                        TRUE)
+      expect_equivalent(all(abs(lder2 - lder2_a) < 1e-07),
+                        TRUE)
+    } # END for r LOOP
+  } # END for dim LOOP
+
+
+  # d # multiple items (one person, two dimensions)
+  for(n_items in 2:4){
+    n_dims       <- 2
+
+    # calculating dims and stuff
+    dims         <- seq_len(n_dims)
+    n_statements <- n_items * n_dims
+    thetas       <- seq(from = 3, to = -3,
+                        length.out = n_dims)
+    alphas       <- rep(x = 1:2,
+                        length.out = n_statements)
+    deltas       <- seq(from = -2, to = 2,
+                        length.out = n_statements)
+    taus         <- rep(x = c(-1, 0, 1),
+                        length.out = n_statements)
+
+    # binding everything together
+    thetas <- rbind(thetas)
+    params <- cbind(alphas, deltas, taus)
+    items  <- cbind(item      = rep(x    = seq_len(n_statements / n_dims),
+                                    each = n_dims),
+                    statement = seq_len(n_statements),
+                    dim       = rep(c(1, 1),
+                                    times = n_items))
+    # creating response options
+    resp   <- create_resp_list(n_dims = n_dims,
+                               n_comb = n_items)
+
+    for(r in resp){
+
+      # estimating likelihood
+      lders   <- estimate_mupp_dlikelihood(thetas = thetas,
+                                           params = params,
+                                           items  = items,
+                                           resp   = rbind(r))
+      lder1   <- lders$gradient
+      lder2   <- lders$hessian
+
+      # actual likelihood
+      lder1_a <- lder1_mupp_rank_impl(thetas, params, items, rbind(r))
+      lder2_a <- lder2_mupp_rank_impl(thetas, params, items, rbind(r))
+
+      # comparing
+      expect_equivalent(all(abs(lder1 - lder1_a) < 1e-07),
+                        TRUE)
+      expect_equivalent(all(abs(lder2 - lder2_a) < 1e-07),
+                        TRUE)
+    } # END for r LOOP
+  } # END for n_items LOOP
+
+  # e # multiple items (one person, one dimension)
+  for(n_items in 2:4){
+
+    n_dims       <- 2
+
+    # calculating dims and stuff
+    dims         <- list(c(1, 1), c(2, 2))[sample(1:2, size = n_items, replace = TRUE)]
+    n_statements <- n_items * n_dims
+    thetas       <- seq(from = 3, to = -3,
+                        length.out = n_dims)
+    alphas       <- rep(x = 1:2,
+                        length.out = n_statements)
+    deltas       <- seq(from = -2, to = 2,
+                        length.out = n_statements)
+    taus         <- rep(x = c(-1, 0, 1),
+                        length.out = n_statements)
+
+    # binding everything together
+    thetas <- rbind(thetas)
+    params <- cbind(alphas, deltas, taus)
+    items  <- cbind(item      = rep(x    = seq_len(n_statements / n_dims),
+                                    each = n_dims),
+                    statement = seq_len(n_statements),
+                    dim       = unlist(dims))
+    # creating response options
+    resp   <- create_resp_list(n_dims = n_dims,
+                               n_comb = n_items)
+
+    for(r in resp){
+
+      # estimating likelihood
+      lders   <- estimate_mupp_dlikelihood(thetas = thetas,
+                                           params = params,
+                                           items  = items,
+                                           resp   = rbind(r))
+      lder1   <- lders$gradient
+      lder2   <- lders$hessian
+
+      # actual likelihood
+      lder1_a <- lder1_mupp_rank_impl(thetas, params, items, rbind(r))
+      lder2_a <- lder2_mupp_rank_impl(thetas, params, items, rbind(r))
+
+      # comparing
+      expect_equivalent(all(abs(lder1 - lder1_a) < 1e-05),
+                        TRUE)
+      expect_equivalent(all(abs(lder2 - lder2_a) < 1e-05),
+                        TRUE)
+    } # END for r LOOP
+  } # END for n_items LOOP
+
+  # f # completely random
+  n_items       <- 50
+  n_dims        <- 5
+  max_item_dims <- 2
+
+  # assume these functions work (check elsewhere)
+  params_sim   <- simulate_mupp_params(n_persons     = 1,
+                                       n_items       = n_items,
+                                       n_dims        = n_dims,
+                                       max_item_dims = 2,
+                                       unidim_items  = TRUE)
+  resp_sim     <- do.call(simulate_mupp_resp, params_sim)
+
+  # transform parameters
+  params_all   <- transform_params_to_mupp(params_sim, resp_sim)
+  thetas       <- params_all$thetas
+  params       <- params_all$params
+  resp         <- params_all$resp
+  items        <- params_all$items
+
+  # estimating likelihood
+  lders   <- estimate_mupp_dlikelihood(thetas = thetas,
+                                       params = params,
+                                       items  = items,
+                                       resp   = resp)
+  lder1   <- lders$gradient
+  lder2   <- lders$hessian
+
+  # actual likelihood
+  lder1_a <- lder1_mupp_rank_impl(thetas, params, items, resp)
+  lder2_a <- lder2_mupp_rank_impl(thetas, params, items, resp)
+
+  # comparing
+  expect_equivalent(all(abs(lder1 - lder1_a) < 1e-05),
+                    TRUE)
+  expect_equivalent(all(abs(lder2 - lder2_a) < 1e-05),
+                    TRUE)
+
+})
